c294a8245f9365eed20d417212bcfbf8b62e2eb8
Armando Rivas <armandorivasv@gmail.com>
2022-04-11 18:10:23 -0400

    agregado componente contador

--- src/components/itemlistcontainer/ItemListContainer.js
+++ src/components/itemlistcontainer/ItemListContainer.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './ItemListContainer.css';
-//import ItemCount from '../itemcount/ItemCount.js';
+import ItemCount from '../itemcount/ItemCount.js';
 import ItemList from '../ItemList/ItemList';
 import { getProducts } from '../../data/data.js'
 import { useParams } from 'react-router-dom';
@@ -20,13 +20,21 @@ const ItemListContainer = (props) => {
     })
 }, [categoryId])
 
+const stockOnAdd = (stockOnAdd) => {
+  alert(`fueron agregados ${stockOnAdd} productos`)
+}
+
   return(
     <div className='container'>
       <h1 className='ItemListContainer text-center'>{props.message}</h1>
       <div>
         <h1 className='display-5 text-center'>{`CATEGORIA: ${categoryId ? categoryId : 'TODAS'}`}</h1>
         <ItemList products={products} />
-      </div>   
+      </div> 
+      <div className='row row-cols-2 g-5 mt-2'>
+          <ItemCount stockInitial={1} stockAvailable={10} onAdd={stockOnAdd} name={'Bicicleta Marlin'} />
+          <ItemCount stockInitial={0} stockAvailable={0} onAdd={stockOnAdd} name={'Bicicleta TopFuel'} />
+        </div>     
   </div>         
   )
 };
